e677758a2624f47ad17386dd49b952230bb93a54
James Yang <jamesyang916@gmail.com>
2018-12-15 01:14:44 -0500

    added multivarate stress test
    Former-commit-id: ba46f09db19f8b32f39affc6ccfc310fa785d5ae
    Former-commit-id: 30c481d24e2255d96d1ce4baa6dc8cd1cbe5f47f
    Former-commit-id: bb4e4cfb2153c7d262fe35a89791fa93c3490ec3
    Former-commit-id: 51e7ed84e67a82076942c4e42ef557f469973d55

--- src/adeval_unittest.cpp
+++ src/adeval_unittest.cpp
@@ -42,5 +42,28 @@ namespace {
         for (int i = 0; i < 5; ++i)
             EXPECT_DOUBLE_EQ(res(1,i), std::cos(x[i]));
     }
+    
+    // Complex Multivariate function
+    // Copy and paste the f,g,... line 
+    // Compilation time increased, but run-time is same
+    TEST(adeval_test, autodiff_multi_complex) {
+        using namespace ad;
+        double x[] = {-1, 2, 3.1, 4.2, 10.2};
+        auto F = std::make_tuple(
+                f, g, f, g, f, g, f, g, f, g, f, g, f, g, f, g,
+                f, g, f, g, f, g, f, g, f, g, f, g, f, g, f, g
+                );
+        arma::Mat<double> res = autodiff(F, x, x + 5);
+        
+        EXPECT_DOUBLE_EQ(res(0,0), std::cos(x[0])*std::cos(x[1]));
+        EXPECT_DOUBLE_EQ(res(0,1), -std::sin(x[0])*std::sin(x[1]));
+        EXPECT_DOUBLE_EQ(res(0,2), 1);
+        EXPECT_DOUBLE_EQ(res(0,3), x[4]);
+        EXPECT_DOUBLE_EQ(res(0,4), x[3]);
+
+        for (int i = 0; i < 5; ++i) {
+            EXPECT_DOUBLE_EQ(res(1,i), std::cos(x[i]));
+        }
+    }
 
 } // end namespace
