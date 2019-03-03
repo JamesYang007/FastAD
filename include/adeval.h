8d45fde4de0536e09afdb014cf858a25c08f6803
James Yang <jamesyang916@gmail.com>
2019-03-03 12:19:19 -0500

    Added rvalue autodiff
    Former-commit-id: 431c2859c7ba4c107bdaa77be7e3ed388a038189
    Former-commit-id: 389fab1849da18137644a5c37a8fafae953dd3f4
    Former-commit-id: 06b1ee4a9aaaa15fe9171569f0f73371963dc16c
    Former-commit-id: 6218f29c7d7b93058b35d3ee235ef5cfe80f5a8c

--- include/adeval.h
+++ include/adeval.h
@@ -23,6 +23,7 @@ inline void autodiff(ExprType&& expr)
 //====================================================================================================
 
 // autodiff on tuple of expressions
+// Lvalue
 template <size_t I = 0, class...ExprType>
 inline typename std::enable_if<I == sizeof...(ExprType), void>::type
 autodiff(std::tuple<ExprType...>& tup) {}
@@ -32,5 +33,14 @@ inline typename std::enable_if<I < sizeof...(ExprType), void>::type
 autodiff(std::tuple<ExprType...>& tup)
 {autodiff(std::get<I>(tup)); autodiff<I+1>(tup);}
 
+// Rvalue
+template <size_t I = 0, class...ExprType>
+inline typename std::enable_if<I == sizeof...(ExprType), void>::type
+autodiff(std::tuple<ExprType...>&& tup) {}
+
+template <size_t I = 0, class... ExprType>
+inline typename std::enable_if<I < sizeof...(ExprType), void>::type
+autodiff(std::tuple<ExprType...>&& tup)
+{autodiff(std::get<I>(tup)); autodiff<I+1>(tup);}
 
 } // end namespace ad
