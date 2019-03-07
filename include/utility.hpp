23d1bd41bda8d001f78db5a0b105ced572a85ed9
James Yang <jamesyang916@gmail.com>
2019-03-07 17:42:09 -0500

    Added utility.hpp dependency. Reorganized admath, utility.
    Moved iterator type_traits to utility.hpp from admath.hpp
    
    
    Former-commit-id: 3139a000ab62df3416f4e72ef8fe661a229b3fd4
    Former-commit-id: 8f31e7dad124692116ab504429f3a51a269a96e7
    Former-commit-id: fa60d0f1a67958be24157afa6e8be519af877ff5
    Former-commit-id: d2000fa22bd7e9dd2e4d360578279cac270cc061

--- include/utility.hpp
+++ include/utility.hpp
@@ -1,18 +1,18 @@
 #include <iterator>
 
-template<typename T, typename = void>
-struct is_iterator
-{
-   static constexpr bool value = false;
-};
+namespace utils {
 
-template<typename T>
-struct is_iterator<
-    T
-    , typename std::enable_if<
-        !std::is_same<typename std::iterator_traits<T>::value_type, void>::value
-        >::type
-    >
-{
-   static constexpr bool value = true;
-};
+    // type_traits extension
+    //
+    // dereferenceable like a pointer
+
+    // Dummy function for return value purpose
+    template <typename T>
+    auto is_pointer_like()
+       -> decltype( * std::declval<T>(), std::true_type{} );
+
+    // Checks if type T is pointer like (dereferenceable)
+    template <typename T>
+    using is_pointer_like_dereferenceable = decltype(is_pointer_like<T>());
+
+} // end utils
