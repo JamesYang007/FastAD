d233cd8ffd6690c57a9e90418aac0127c5e3ef83
James Yang <jamesyang916@gmail.com>
2019-03-28 20:21:58 -0400

    New adcompose_unittest. Intel MKL VS
    Former-commit-id: e28bc558db56f2b1e796b7df19e164776d572a25
    Former-commit-id: 0fd50180fa04fc9c208aeaadadb85bc18973adb3
    Former-commit-id: ff883d8ef9735ad7c7ce89d6aecb82966c497852 [formerly a6a3ed348319441eaa6ed1429f894bf82743cb3d] [formerly e0b2d89e8152566e944b6afa14384e9418f024de] [formerly 66cded48d9327ee2fdaef0f1232108d706607068 [formerly 5e5744e4760c15fe44ab3a63ac9b7eb9c313c185] [formerly e0b2d89e8152566e944b6afa14384e9418f024de [formerly b87ab2c7947581051e43959db999a1c94c17955e]]]
    Former-commit-id: 13d7d55b1279c5d4f7df9b1cbc1a1139771b7349 [formerly 2bc0613564417bbf513cdcdf7198ac6028b51f34] [formerly c184d4ccb132d71b219749f9099099d1d4fdc55a [formerly 3483d8bd9bb2b72be43540d03a6bd6601e781778]]
    Former-commit-id: c184d4ccb132d71b219749f9099099d1d4fdc55a
    Former-commit-id: 07d39f163021c74ffea98dd9595d13c5d749e3f3
    Former-commit-id: 9549e759d237e599817e1f7c0b632cb7f841f0e0 [formerly 8ee3868226b43293f83fa31ce3fefd46ebd4b361]
    Former-commit-id: c7021eebc4a8ec48b5130ee0c158a17c3dd615ae
    Former-commit-id: b3e58e10752b54b4d4a3c16f5748591549c61021
    Former-commit-id: 39400a808312abd74aff5e27b5a1c4890cb2bd67 [formerly 0036aa144bfa7adf4e2c738877c2ee42999d72aa] [formerly 30ed50a49cc017e3a68e3eaacd1f4660ab03879c] [formerly 30ed50a49cc017e3a68e3eaacd1f4660ab03879c [formerly 59dc5d36e76fd908e58efe99b278016a2b055063]] [formerly 8b8b689f34c9bfd538af42fc7a1d85472d2ee2c1 [formerly 573ac8d0ed71c70e6c84e19b3de8993ce6e0538d] [formerly 5558c603e5a67bb5e35b61cbf71edd17c11aa55e] [formerly 30ed50a49cc017e3a68e3eaacd1f4660ab03879c [formerly 59dc5d36e76fd908e58efe99b278016a2b055063] [formerly 5558c603e5a67bb5e35b61cbf71edd17c11aa55e [formerly 7fabfe76b16f9e5b37bfedc9b122220a4bd92be8]]]]
    Former-commit-id: c3d8fb05ba10c65303a3cb2050984296b50dac7b [formerly cebc57335b241e327cfc4df1739dd082d77f272d] [formerly 73a5975c891d6dede44b4ef4295cb98c4630ca00 [formerly 9bb6f2d954a41ac62b404a22c7f245569a2f17e3]] [formerly ba87fda97aebfb511a0c00dddd1477f282cee694 [formerly 377acba54a64655814b8d3c22460b896efff839f] [formerly 9bb6f2d954a41ac62b404a22c7f245569a2f17e3 [formerly 2e3e5d38f53ad5e6f8096f05f3ddb5ad9a915b21]]]
    Former-commit-id: ba87fda97aebfb511a0c00dddd1477f282cee694 [formerly 377acba54a64655814b8d3c22460b896efff839f]
    Former-commit-id: b5f1b236abc4373444c720df57dd115ae6f4147b [formerly ba87fda97aebfb511a0c00dddd1477f282cee694]
    Former-commit-id: 11e706657bac282fb23ad361593b754f0dfce0df

---
+++ src/adcompose_unittest.cpp
@@ -0,0 +1,86 @@
+#define _USE_MATH_DEFINES
+
+#include "adeval.hpp"
+#include "admath.hpp"
+#include "adjacobian.hpp"
+#include "gtest/gtest.h"
+#include <armadillo>
+
+namespace {
+
+	auto F_lmda = MAKE_LMDA(
+		ad::sin(x[0]) + x[1],
+		w[0] * x[1]
+	);
+
+	auto G_lmda = MAKE_LMDA(
+		ad::cos(x[0])
+	);
+
+	auto H_lmda = MAKE_LMDA(
+		ad::log(x[0])
+	);
+
+	auto F = ad::make_function(F_lmda);
+	auto G = ad::make_function(G_lmda);
+	auto H = ad::make_function(H_lmda);
+
+	// Scalar of Scalar(x_0, x_1)
+	TEST(adcompose, scalar_scalar) {
+		using namespace ad;
+		double x[] = { M_PI, 1. };
+		auto G_F = compose(G, F);
+		auto&& expr = G_F(x, x + 2);
+		autodiff(expr);
+
+		double tmp = -std::sin((std::sin(x[0]) + x[1])*x[1]);
+		EXPECT_DOUBLE_EQ(G_F.x[0].df, tmp*std::cos(x[0])*x[1]);
+		EXPECT_DOUBLE_EQ(G_F.x[1].df, tmp*(2 * x[1] + std::sin(x[0])));
+	}
+
+	// Scalar of Scalar of Scalar(x_0, x_1)
+	TEST(adcompose, scalar_scalar_scalar) {
+		using namespace ad;
+		double x[] = { 0.268* M_PI, 2.3 };
+		auto H_G_F = compose(H, G, F);
+		auto&& expr = H_G_F(x, x + 2);
+		auto f_val = autodiff(expr);
+
+		double tmp = -std::sin((std::sin(x[0]) + x[1])*x[1]);
+		double tmp2 = std::exp(-f_val);
+		EXPECT_DOUBLE_EQ(H_G_F.x[0].df, tmp2 * tmp * std::cos(x[0])*x[1]);
+		EXPECT_DOUBLE_EQ(H_G_F.x[1].df, tmp2 * tmp * (2 * x[1] + std::sin(x[0])));
+	}
+
+	// Scalar of Vector(x_0, x_1)
+	TEST(adcompose, scalar_vector) {
+		using namespace ad;
+		double x[] = { 0.268 * M_PI, 2.3 };
+		auto FG = make_function(F_lmda, G_lmda);
+		auto F_FG = compose(F, FG);
+
+		auto&& expr = F_FG(x, x + 2);
+		autodiff(expr);
+
+		// check
+		double x_tilde[2] = { 0 };
+		x_tilde[0] = Evaluate(F(x, x + 2));
+		x_tilde[1] = Evaluate(G(x, x + 2));
+
+		arma::Mat<double> grad_F;
+		arma::Mat<double> jacobi_FG;
+		jacobian(grad_F, x_tilde, x_tilde + 2, F_lmda);
+		jacobian(jacobi_FG, x, x + 2, F_lmda, G_lmda);
+		arma::Mat<double> comp = grad_F * jacobi_FG;
+
+		EXPECT_DOUBLE_EQ(F_FG.x[0].df, comp(0, 0));
+		EXPECT_DOUBLE_EQ(F_FG.x[1].df, comp(0, 1));
+	}
+
+	// Vector of Vector
+	TEST(adcompose, vector_vector) {
+		using namespace ad;
+
+	}
+
+} // end namespace 
