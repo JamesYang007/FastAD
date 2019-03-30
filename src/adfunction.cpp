01369f2b2bc573f5f9ca2776367a441c52119d37
James Yang <jamesyang916@gmail.com>
2019-03-30 15:40:03 -0400

    Added new adfunction to test construction of Function objects.
    Former-commit-id: 2c8615766cdd8a6d3e4bc9a83d56514ede91f7f5
    Former-commit-id: 601734bc39abfae3459f5beab8d6e5e84a74b3a2
    Former-commit-id: e8e5c4028300875b4aae267ef763b728bcfcda6d [formerly 62a5b964e71de83ead029a62afeaf487ee98120c] [formerly 55555afd4614cdd0ab9d327b71043e9ba0d3f450] [formerly a3e32a6840cb907f375c76b6805bdac98a31b234 [formerly b665bc6687852ae93bd661d2292b398b9edfe2af] [formerly 55555afd4614cdd0ab9d327b71043e9ba0d3f450 [formerly 46ba4c72d45023a30cc6fc1cc2da1819b2d5bd53]]]
    Former-commit-id: 5b0c75b1d3c6756299bbd9a3cdde8e73c071e9bc [formerly 5bcadf972575ad4276ac0f8825bdfc7309ae84cf] [formerly 7a52d890d324164e95395b3f0e7e7bf690c73eac [formerly 0c0b923f2e0e55c78727e44b22b86978999e96c1]]
    Former-commit-id: 7a52d890d324164e95395b3f0e7e7bf690c73eac
    Former-commit-id: 27095379f9a7058805c8efcfb845e464f19f9c82
    Former-commit-id: b1227dcc341cd2fd52e85c593f9a7b665e80bfc9 [formerly d4276c5115cdabe45f67aeadd6fd91a00f8022b1]
    Former-commit-id: 2a06d9c13cb03042f90b3f9aa50ff21f6202d416
    Former-commit-id: 1f6f230c5e4db2a22281700fee73fda4698daf39
    Former-commit-id: 17b8465265ab11d0893acce1469086c796a5dedc [formerly bbcddaed25115ab70042298a86822ca70610a0f1] [formerly cfd3375f45d83fb904a59b75854a870d8e1d5a17] [formerly d7250bedd5092129eccbf1a60d068f1123472c17 [formerly d036fadcccc11df0f96905c8a7c0d2d829bc87ad] [formerly cfd3375f45d83fb904a59b75854a870d8e1d5a17 [formerly 6c6fcaecb74a48bc55fa714742f86aa01208755f]]] [formerly f560be26bfb068b5a30b8ded22347c8c66a0c073 [formerly 061f77c5fab1ba078b3906139a7ef1673a54cd9d] [formerly 5ded2393218a9989381ba421a2e2700b480b2218] [formerly cfd3375f45d83fb904a59b75854a870d8e1d5a17 [formerly 6c6fcaecb74a48bc55fa714742f86aa01208755f] [formerly 5ded2393218a9989381ba421a2e2700b480b2218 [formerly b74701f7746643f39621a72e4217e5b5563d101d]]]]
    Former-commit-id: 202ac2e167e1b1b72d3ef06fe046d09b8fe11289 [formerly 0be5394b6aaa917841bbd4f4c2f2ae7a51491dc1] [formerly 91351fcaa033787cb554af558cfb83883859cf01] [formerly 048097255572a06108883a95ac9a2503c26349e9 [formerly 3fb26afe55cdb2d61c43d1cff57d16888d0a08ec] [formerly 91351fcaa033787cb554af558cfb83883859cf01 [formerly f3f41ca6f7204637cbc4070ed42be0d2e9aa5d58]]]
    Former-commit-id: 048097255572a06108883a95ac9a2503c26349e9 [formerly 3fb26afe55cdb2d61c43d1cff57d16888d0a08ec]
    Former-commit-id: c61809eb6d1c6aa1d6de270f0c40d037490e6100 [formerly 048097255572a06108883a95ac9a2503c26349e9]
    Former-commit-id: f3fe2aeb170c1921ac5e4d572acdf87f880e057b

---
+++ src/adfunction.cpp
@@ -0,0 +1,122 @@
+#include "adeval.hpp"
+#include "admath.hpp"
+#include "adfunction.hpp"
+#include "gtest/gtest.h"
+
+namespace {
+
+	auto F_lmda = MAKE_LMDA(ad::sin(x[0]));
+	auto G_lmda = MAKE_LMDA(ad::cos(x[0]));
+
+	// Scalar
+	TEST(adfunction, scalar_copy_ctor) {
+		using namespace ad;
+		double x[] = { 1. };
+		auto F = make_function(F_lmda);
+		auto G = F; // copy construct
+
+		// Check that x,w are independent for F,G
+		autodiff(G(x, x + 1));
+		EXPECT_EQ(F.x.size(), 0);
+		EXPECT_EQ(F.w.size(), 0);
+		EXPECT_EQ(G.x.size(), 1);
+		EXPECT_EQ(G.w.size(), 1);
+
+		// Check that autodiff results are the same
+		autodiff(F(x, x + 1));
+		EXPECT_DOUBLE_EQ(F.x[0].w, G.x[0].w);
+		EXPECT_DOUBLE_EQ(F.x[0].df, G.x[0].df);
+	}
+
+	// Vector
+	TEST(adfunction, vector_copy_ctor) {
+		using namespace ad;
+		double x[] = { 1. };
+		auto F = make_function(F_lmda);
+		auto G = make_function(G_lmda);
+		auto FG = make_function(F, G);
+
+		EXPECT_NE(&(std::get<0>(FG.tup)), &F);
+		EXPECT_NE(&(std::get<1>(FG.tup)), &G);
+
+		auto&& expr = FG(x, x + 1);
+		autodiff(expr);
+
+		EXPECT_EQ(F.x.size(), 0);
+		EXPECT_EQ(F.w.size(), 0);
+		EXPECT_EQ(G.x.size(), 0);
+		EXPECT_EQ(G.w.size(), 0);
+		EXPECT_EQ(std::get<0>(FG.tup).x.size(), 1);
+		EXPECT_EQ(std::get<0>(FG.tup).w.size(), 1);
+		EXPECT_EQ(std::get<1>(FG.tup).x.size(), 1);
+		EXPECT_EQ(std::get<1>(FG.tup).w.size(), 1);
+
+		EXPECT_NE(std::get<0>(FG.tup).x.size(), F.x.size());
+		EXPECT_NE(std::get<1>(FG.tup).x.size(), G.x.size());
+		EXPECT_NE(std::get<0>(FG.tup).w.size(), F.w.size());
+		EXPECT_NE(std::get<1>(FG.tup).w.size(), G.w.size());
+
+	}
+
+	// ComposedFunction (scalar)
+	TEST(adfunction, composed_scalar_ctor) {
+		using namespace ad;
+		double x[] = { 1. };
+		auto F = make_function(F_lmda);
+		auto G = make_function(G_lmda);
+		auto F_G = compose(F, G);
+		autodiff(F_G(x, x + 1));
+
+		EXPECT_NE(&F_G.composer, &F);
+		EXPECT_NE(&F_G.composed, &G);
+
+		EXPECT_EQ(F.x.size(), 0);
+		EXPECT_EQ(F.w.size(), 0);
+		EXPECT_EQ(G.x.size(), 0);
+		EXPECT_EQ(G.w.size(), 0);
+
+		EXPECT_EQ(F_G.composer.w.size(), 1);
+		EXPECT_EQ(F_G.composed.w.size(), 1);
+		EXPECT_EQ(F_G.x.size(), 1);
+		EXPECT_EQ(F_G.w.size(), 1);
+	}
+
+	// ComposedFunction (vector)
+	TEST(adfunction, composed_vector_ctor) {
+		using namespace ad;
+		double x[] = { 0.2, 1.59 };
+		auto F_lmda = MAKE_LMDA(
+			ad::sin(x[0]) + x[1],
+			w[0] * x[1]
+		);
+
+		auto G_lmda = MAKE_LMDA(
+			ad::cos(x[0])
+		);
+
+		auto H_lmda = MAKE_LMDA(
+			ad::log(x[0])
+		);
+		auto FG = make_function(F_lmda, G_lmda);
+		auto GH = make_function(G_lmda, H_lmda);
+		auto FG_GH = compose(FG, GH);
+		auto&& expr = FG_GH(x, x + 2);
+		autodiff(expr);
+
+		EXPECT_NE(&(std::get<0>(FG_GH.tup).composed), &GH);
+		EXPECT_NE(&(std::get<1>(FG_GH.tup).composed), &GH);
+		EXPECT_NE(&(std::get<0>(FG_GH.tup).composer), &(std::get<0>(FG.tup)));
+		EXPECT_NE(&(std::get<1>(FG_GH.tup).composer), &(std::get<1>(FG.tup)));
+
+		EXPECT_EQ(std::get<0>(FG.tup).x.size(), 0);
+		EXPECT_EQ(std::get<1>(FG.tup).x.size(), 0);
+		EXPECT_EQ(std::get<0>(GH.tup).x.size(), 0);
+		EXPECT_EQ(std::get<1>(GH.tup).x.size(), 0);
+
+		EXPECT_EQ(std::get<0>(FG.tup).w.size(), 0);
+		EXPECT_EQ(std::get<1>(FG.tup).w.size(), 0);
+		EXPECT_EQ(std::get<0>(GH.tup).w.size(), 0);
+		EXPECT_EQ(std::get<1>(GH.tup).w.size(), 0);
+	}
+
+} // end namespace 
