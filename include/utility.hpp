8773c1a5d88f144722aa8c34a5b4516e3a57f650
James Yang <jamesyang916@gmail.com>
2019-04-01 19:50:45 -0400

    Generalized glue_many, zip_func, init_tup with universal reference.
    Utility now has pragma once and contains helper is_tuple.
    
    
    Former-commit-id: bfa9f08a063660eb98e352023afe4862d54916be
    Former-commit-id: af6ee1d0c8d987e7595a8c715d221d549c4415a2
    Former-commit-id: 4c13c020ad2a510df434d3fba5cd5c6f4949db9e [formerly 1a9ccea5f25c13dc719c140a887376c7a7a2a992] [formerly ec6bc7e5c7ca3a938760ee7253a6737cb7cab26c] [formerly de68412ad17ff602a05992f4dadaf1099ea0cfc9 [formerly 33e4c3b292edbb252c68a865b7e5b26ce05948a4] [formerly ec6bc7e5c7ca3a938760ee7253a6737cb7cab26c [formerly 9ac50178d7eb117cc4c52992aa8ab0ddc4607eed]]]
    Former-commit-id: 6756fe2e4bd4d301589d6b70ca46bb9067930cd6 [formerly 3b363a29701bf16ca70bd776a071237e5e4889f0] [formerly 7289c836e1775be192887999b456f76f6d64e8c5 [formerly 84d8eaeaddc8bc69c779b4a3959cbe0c814b26ad]]
    Former-commit-id: 577882973c6cf876568eee1ffb77491a086cdee4 [formerly 7289c836e1775be192887999b456f76f6d64e8c5]
    Former-commit-id: 4108c140b1ea27fa68c7819e78013561b14869e7
    Former-commit-id: 9c1a077da0985e38706a6307ae0043d09e15475f [formerly 7cf19100bbbbf8f2b6d987b6663a22cddffdbe54]
    Former-commit-id: fbb83fdc016db3cf01b1597ea2288179b7f75d59
    Former-commit-id: 7ca0375f4d43b90b4f762157dc15edf2940bbfe1
    Former-commit-id: 1f918a3fd4316676d705f8d2ab46eb8e27cbccf7 [formerly ff667930a050857fc2dfa69ea2bc09c2ebe2322d] [formerly 5626cef418f5d0ab5206c664d9e3a2853f998258 [formerly 4365c71d4ebcea92e7ac83f698d4d2734ac00ba6]] [formerly 4365c71d4ebcea92e7ac83f698d4d2734ac00ba6 [formerly e75816bee78a247dff588a277ba989a7c9883649]] [formerly f79aaef5199577547ce855e4930bedf5e8f128db [formerly e7b141e9f858440b42bf655111a8afe57aa39ebd] [formerly 5df55d25ae64d3d06313768afa166754d51f6be5] [formerly 4365c71d4ebcea92e7ac83f698d4d2734ac00ba6 [formerly e75816bee78a247dff588a277ba989a7c9883649] [formerly 5df55d25ae64d3d06313768afa166754d51f6be5 [formerly cac1c467d5043a352741cb4a7a109c6019e9fb2a]]]]
    Former-commit-id: 5eddb703f0f3f169d9bf591367af88f24edeb2e2 [formerly 4c608fa9ddd3a1bcc314dd93de9bb0dcbb2c1b5e] [formerly 28207e4ac9719d4962381ce451b2adbfa413ff8a] [formerly f290805260eb1433e5f17a517ff9e6372d5a3add [formerly 64b87937692f70985d34fd55351fda7a1001f86a] [formerly 28207e4ac9719d4962381ce451b2adbfa413ff8a [formerly cbb8b1daeed570893cd1a52f2059ab5f28d16cdf]]]
    Former-commit-id: f290805260eb1433e5f17a517ff9e6372d5a3add [formerly 64b87937692f70985d34fd55351fda7a1001f86a]
    Former-commit-id: f87736f5af094fc899599c127f0b18197126d5fe [formerly f290805260eb1433e5f17a517ff9e6372d5a3add]
    Former-commit-id: b2daea040733a9bc5a738beb4f3a6358e70352ca

--- include/utility.hpp
+++ include/utility.hpp
@@ -1,18 +1,31 @@
+#pragma once
 #include <iterator>
 
 namespace utils {
 
-    // type_traits extension
-    //
-    // dereferenceable like a pointer
+	// type_traits extension
+	//
+	// dereferenceable like a pointer
 
-    // Dummy function for return value purpose
-    template <typename T>
-    auto is_pointer_like()
-       -> decltype( * std::declval<T>(), std::true_type{} );
+	// Dummy function for return value purpose
+	template <typename T>
+	auto is_pointer_like()
+		-> decltype(*std::declval<T>(), std::true_type{});
 
-    // Checks if type T is pointer like (dereferenceable)
-    template <typename T>
-    using is_pointer_like_dereferenceable = decltype(is_pointer_like<T>());
+	// Checks if type T is pointer like (dereferenceable)
+	template <typename T>
+	using is_pointer_like_dereferenceable = decltype(is_pointer_like<T>());
+
+	// Checks if type T is a tuple
+	template <class T>
+	struct is_tuple { static constexpr bool value = false; };
+	template <class... Ts>
+	struct is_tuple<std::tuple<Ts...>> { static constexpr bool value = true; };
+	template <class... Ts>
+	struct is_tuple<std::tuple<Ts...>&> { static constexpr bool value = true; };
+	template <class... Ts>
+	struct is_tuple<std::tuple<Ts...> const> { static constexpr bool value = true; };
+	template <class... Ts>
+	struct is_tuple<std::tuple<Ts...> const&> { static constexpr bool value = true; };
 
 } // end utils
