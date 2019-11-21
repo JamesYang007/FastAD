d9a0d6544a5dc8ec8f1b19371a8c9eb06ade9839
James Yang <jamesyang916@gmail.com>
2019-11-21 11:28:47 -0500

    Fix multithreading to use boost thread_pool in adeval

--- include/adeval.hpp
+++ include/adeval.hpp
@@ -1,113 +1,120 @@
 #pragma once
 #include <thread>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio.hpp>
 #include "adnode.hpp"
 
 namespace ad {
 
-	// Glue then evaluate
-	// Forward propagation
-	template <class ExprType>
-	inline auto Evaluate(ExprType&& expr)
-	{
-		return expr.feval();
-	}
+static constexpr size_t THR_THRESHOLD = 10;
 
-	// Backward propagation
-	template <class ExprType>
-	inline void EvaluateAdj(ExprType&& expr)
-	{
-		expr.beval(1);
-	}
+// Glue then evaluate
+// Forward propagation
+template <class ExprType>
+inline auto Evaluate(ExprType&& expr)
+{
+    return expr.feval();
+}
 
-	// Both forward and backward
-	// This is primarily for details_tuple namespace usage
-	// std::thread argument needs any overloaded function to be specified
-	// workaround: create a non-overloaded fn and autodiff will call this
-	template <class ExprType>
-	inline auto EvaluateBoth(ExprType&& expr)
-	{
-		auto t = Evaluate(std::forward<ExprType>(expr));
-		EvaluateAdj(std::forward<ExprType>(expr));
-		return t;
-	}
+// Backward propagation
+template <class ExprType>
+inline void EvaluateAdj(ExprType&& expr)
+{
+    expr.beval(1);
+}
 
-	template <class ExprType>
-	inline auto autodiff(ExprType&& expr)
-	{
-		return EvaluateBoth(std::forward<ExprType>(expr));
-	}
+// Both forward and backward
+// This is primarily for details_tuple namespace usage
+// std::thread argument needs any overloaded function to be specified
+// workaround: create a non-overloaded fn and autodiff will call this
+template <class ExprType>
+inline auto EvaluateBoth(ExprType&& expr)
+{
+    auto t = Evaluate(expr);
+    EvaluateAdj(expr);
+    return t;
+}
 
-	//====================================================================================================
+template <class ExprType>
+inline auto autodiff(ExprType&& expr)
+{
+    return EvaluateBoth(expr);
+}
 
-	// autodiff on tuple of expressions
-	namespace details_tuple {
+//====================================================================================================
 
-		// No multi-threading
-		// Lvalue
-		template <size_t I, class...ExprType>
-		inline typename std::enable_if<I == sizeof...(ExprType), void>::type
-			autodiff_(std::tuple<ExprType...>& tup) {}
+// autodiff on tuple of expressions
+namespace details_tuple {
 
-		template <size_t I, class... ExprType>
-		inline typename std::enable_if < I < sizeof...(ExprType), void>::type
-			autodiff_(std::tuple<ExprType...>& tup)
-		{
-			ad::autodiff(std::get<I>(tup)); autodiff_<I + 1>(tup);
-		}
+// No multi-threading
+// Lvalue
+template <size_t I, class...ExprType>
+inline typename std::enable_if<I == sizeof...(ExprType), void>::type
+    autodiff_(std::tuple<ExprType...>& tup) {}
 
-		// multi-threading
-		template <class ExprType>
-		inline void autodiff_(ExprType& expr)
-		{
-			std::thread thr(ad::EvaluateBoth<ExprType>, expr);
-			thr.join();
-		}
+template <size_t I, class... ExprType>
+inline typename std::enable_if < I < sizeof...(ExprType), void>::type
+    autodiff_(std::tuple<ExprType...>& tup)
+{
+    ad::autodiff(std::get<I>(tup)); autodiff_<I + 1>(tup);
+}
 
-		template <class ExprType1, class...ExprType>
-		inline void autodiff_(ExprType1& expr1, ExprType&... expr)
-		{
-			std::thread thr(ad::EvaluateBoth<ExprType1>, expr1);
-			autodiff_(expr...);
-			thr.join();
-		}
+// multi-threading using boost thread_pool
+template <class ExprType>
+inline void autodiff_(boost::asio::thread_pool& pool, ExprType& expr)
+{
+    boost::asio::post(pool, [&](){
+            ad::EvaluateBoth(expr);
+            });
+}
 
-		// true_type
-		template <class...ExprType, size_t...I>
-		inline void autodiff_(std::tuple<ExprType...>& tup, std::index_sequence<I...>)
-		{
-			autodiff_(std::get<I>(tup)...);
-		}
+template <class ExprType1, class...ExprType>
+inline void autodiff_(boost::asio::thread_pool& pool, ExprType1& expr1, ExprType&... expr)
+{
+    boost::asio::post(pool, [&](){
+            ad::EvaluateBoth(expr1);
+            });
+    autodiff_(pool, expr...);
+}
 
-		template <class...ExprType>
-		inline void autodiff_(std::tuple<ExprType...>& tup, std::true_type)
-		{
-			autodiff_(tup, std::make_index_sequence<sizeof...(ExprType)>());
-		}
+template <class...ExprType, size_t...I>
+inline void autodiff_(std::tuple<ExprType...>& tup, std::index_sequence<I...>)
+{
+    const int thread_num = std::thread::hardware_concurrency();
+    boost::asio::thread_pool pool(thread_num);
+    autodiff_(pool, std::get<I>(tup)...);
+    pool.join();
+}
 
-		// false_type
-		template <class...ExprType>
-		inline void autodiff_(std::tuple<ExprType...>& tup, std::false_type)
-		{
-			autodiff_<0>(tup);
-		}
+// use multi-threading to compute autodiff on tuple of expressions
+template <class...ExprType>
+inline void autodiff_(std::tuple<ExprType...>& tup, std::true_type)
+{
+    autodiff_(tup, std::make_index_sequence<sizeof...(ExprType)>());
+}
 
-	}
+// false_type
+template <class...ExprType>
+inline void autodiff_(std::tuple<ExprType...>& tup, std::false_type)
+{
+    autodiff_<0>(tup);
+}
 
-	constexpr size_t THR_THRESHOLD = 1000;
+} // namespace details_tuple
 
-	// USER-CALLABLE autodiff 
-	template <class...ExprType>
-	inline void autodiff(std::tuple<ExprType...>& tup)
-	{
-		details_tuple::autodiff_(tup
-			, std::integral_constant<bool, (sizeof...(ExprType) >= THR_THRESHOLD)>());
-	}
+// USER-CALLABLE autodiff 
+template <class...ExprType>
+inline void autodiff(std::tuple<ExprType...>& tup)
+{
+    details_tuple::autodiff_(tup
+        , std::integral_constant<bool, (sizeof...(ExprType) >= THR_THRESHOLD)>());
+}
 
-	template <class...ExprType>
-	inline void autodiff(std::tuple<ExprType...>&& tup)
-	{
-		details_tuple::autodiff_(tup
-			, std::integral_constant<bool, (sizeof...(ExprType) >= THR_THRESHOLD)>());
-	}
+template <class...ExprType>
+inline void autodiff(std::tuple<ExprType...>&& tup)
+{
+    details_tuple::autodiff_(tup
+        , std::integral_constant<bool, (sizeof...(ExprType) >= THR_THRESHOLD)>());
+}
 
 } // end namespace ad
