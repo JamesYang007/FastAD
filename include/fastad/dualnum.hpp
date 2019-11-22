3cec25196de378fc589eb8d64acede9a4aa6fe5d
James Yang <jamesyang916@gmail.com>
2019-11-22 11:57:06 -0500

    Add better comments in dualnum and utility

--- include/fastad/dualnum.hpp
+++ include/fastad/dualnum.hpp
@@ -3,16 +3,19 @@
 namespace ad {
 namespace core {
 
-    // Node data container
-    template <class T>
-    struct DualNum
-    {
-        using value_type = T;
-        T w, df;   
-        DualNum(T w, T df)
-            : w(w), df(df)
-        {}
-    };
+// Underlying data structure containing value and adjoint.
+// See ADForward or ADNode class template.
+// Represent value as "w" and adjoint as "df".
+// @tparam T    underlying data type (ex. double)
+template <class T>
+struct DualNum
+{
+    using value_type = T;
+    T w, df;   
+    DualNum(T w, T df)
+        : w(w), df(df)
+    {}
+};
 
 } // namepsace core
 } // namespace ad 
