3884faa64d656722580dbdbb0d4061eee39dadce
Kent <kentjhall3@hotmail.com>
2019-12-03 15:43:52 -0500

    overhauled mat.hpp to be proper (one vector), also now has equality operators

--- include/fastad_bits/mat.hpp
+++ include/fastad_bits/mat.hpp
@@ -8,25 +8,28 @@ template <typename T>
 class Mat;
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const Mat<T>& mat);
+template <typename T>
+bool operator==(const Mat<T>& mat1, const Mat<T>& mat2);
 
 template <typename T>
 class Mat {
 
 public:
 
-	Mat() : data() {}
-	Mat(const Mat& mat) : data(mat.data) {}
-	Mat(const std::vector< std::vector<T> >& dat) : data(dat) {}
-	Mat(size_t size, T fill) : data(size, std::vector<T>(size, fill)) {}
-	Mat(size_t rows, size_t cols, T fill) : data(rows, std::vector<T>(cols, fill)) {}
+	Mat() : data(), rows_(0), cols_(0) {}
+	Mat(const Mat& mat) : data(mat.data), rows_(mat.rows_), cols_(mat.cols_) {}
+	Mat(size_t size, T fill) : data(size*size, fill), rows_(size), cols_(size) {}
+	Mat(size_t rows, size_t cols, T fill) : data(rows*cols, fill), rows_(rows), cols_(cols) {}
 
-	friend std::ostream& operator<< <T>(std::ostream& os, const Mat& mat);
-	std::vector<T>& operator[](int i) { return data[i];  }
-	const std::vector<T>& operator[](int i) const { return ((Mat&)*this)[i]; }
+	friend std::ostream& operator<<<>(std::ostream& os, const Mat& mat);
+	friend bool operator==<>(const Mat& mat1, const Mat& mat2);
+	friend bool operator!=(const Mat& mat1, const Mat& mat2) { return !(mat1 == mat2); }
+	T& operator()(size_t row, size_t col) { return data[row*cols_ + col];  }
+	const T& operator()(size_t row, size_t col) const { return ((Mat&)*this)(row, col); }
 
-	const std::vector< std::vector<T> >& vecs() const { return data; }
-	size_t rows() const { return data.size(); }
-	size_t cols() const { return data.begin() != data.end() ? data.begin()->size() : 0; }	
+	size_t size() const { return rows_*cols_; }
+	size_t n_rows() const { return rows_; }
+	size_t n_cols() const { return cols_; }
 
 	void print(std::ostream& os, std::string header) { os << header << std::endl << *this; }
 	void print(std::string header) { print(std::cout, header);  }
@@ -36,61 +39,58 @@ public:
 
 	Mat t();	
 
-	class iterator {
+	using iterator = typename std::vector<T>::iterator;
+	using const_iterator = typename std::vector<T>::const_iterator;
 
-	public:
-
-		iterator(typename std::vector<T>::iterator it, typename std::vector< std::vector<T> >::iterator row_it, typename std::vector< std::vector<T> >::iterator row_it_end)
-			: iter(it), row_iter(row_it), row_end_iter(row_it_end) {}
-		~iterator() {}
-		iterator& operator=(const iterator& it) { iter = it.iter; row_iter = it.row_iter; row_end_iter = it.row_end_iter; return *this; }
-		iterator& operator++() { if (++iter == row_iter->end() && ++row_iter != row_end_iter) { iter = row_iter->begin(); } return *this; }
-		T& operator*() const { return *iter; }
-		bool operator==(const iterator& it) const { return row_iter == it.row_iter && iter == it.iter; }
-		bool operator!=(const iterator& it) const { return !(*this == it); }
-
-	private:
-
-		typename std::vector<T>::iterator iter;				
-		typename std::vector< std::vector<T> >::iterator row_iter;
-		typename std::vector< std::vector<T> >::iterator row_end_iter;
-
-	};
-
-	iterator begin() { return iterator(data.begin()->begin(), data.begin(), data.end()); }
-	iterator end() { return iterator((data.end() - 1)->end(), data.end(), data.end()); }
-	const iterator begin() const { return ((Mat&)*this).begin(); }
-	const iterator end() const { return ((Mat&)*this).end(); }
-
+	iterator begin() { return data.begin(); }
+	const_iterator begin() const { return data.begin(); }
+	iterator end() { return data.end(); }
+	const_iterator end() const { return data.end(); }
+	
 private:
 
-	std::vector< std::vector<T> > data;	
+	Mat(const std::vector<T>& dat, size_t rows, size_t cols) : data(dat), rows_(rows), cols_(cols) {}
 
+	std::vector<T> data;	
+	size_t rows_, cols_;
 };
 
 template <typename T>
-Mat<T> Mat<T>::t() {
-	std::vector< std::vector<T> > cols(this->cols(), std::vector<T>());
-
-	typename std::vector< std::vector<T> >::iterator col_it;
-	for (const std::vector<T>& row : this->vecs()) {
-		col_it = cols.begin();
-		for (const T& item : row) {
-			col_it->push_back(item);
-			++col_it;
-		}
+std::ostream& operator<<(std::ostream& os, const Mat<T>& mat) {
+	size_t i = 0;
+	for (const T& item : mat) {
+		os << item << '\t';
+		if (++i >= mat.cols_) { os << std::endl; i = 0; }
 	}
-
-	return Mat<T>(cols);
+	return os;
 }
 
 template <typename T>
-std::ostream& operator<<(std::ostream& os, const Mat<T>& mat) {
-	for (const std::vector<T>& row : mat.vecs()) {
-		for (const T& item : row) { os << item << '\t'; }
-		os << std::endl;
+bool operator==(const Mat<T>& mat1, const Mat<T>& mat2) {
+	if (mat1.rows_ != mat2.rows_ || mat1.cols_ != mat2.cols_) { return false; }	
+
+	typename Mat<T>::const_iterator it = mat2.begin();
+	for (const T& item : mat1) {
+		if (item != *it++) { return false; }
 	}
-	return os;
+
+	return true;
+}
+
+template <typename T>
+Mat<T> Mat<T>::t() {
+	std::vector<T> data_t;
+
+	Mat<T>::iterator it;
+	for (size_t c = 0; c < this->cols_; c++) {
+		it = this->begin() + c;
+		while (std::distance(it, this->end()) > 0) {
+			data_t.push_back(*it);
+			it += this->cols_;
+		}
+	}
+
+	return Mat<T>(data_t, this->cols_, this->rows_);
 }
 
 }
