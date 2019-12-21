b114083be8ac553cb02dee015e4ebed6628d73b2
James Yang <jamesyang916@gmail.com>
2019-12-21 16:53:08 -0500

    Update setup to not make directory build

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 # Setup google benchmark and googletest
 if [ ! -d "libs/benchmark/googletest" ]; then
-    mkdir build
     git clone https://github.com/google/googletest.git libs/benchmark/googletest
     cd libs/benchmark
     mkdir build && cd build
