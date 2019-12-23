6dd9f7abfb5691cae1a6308fd00230d06a4bf567
James Yang <jamesyang916@gmail.com>
2019-12-22 21:58:01 -0500

    Remove setup with release mode

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ if [ ! -d "libs/benchmark/googletest" ]; then
     git clone https://github.com/google/googletest.git libs/benchmark/googletest
     cd libs/benchmark
     mkdir build && cd build
-    cmake ../ -DCMAKE_BUILD_TYPE=Release -DBENCHMARK_ENABLE_LTO=true
+    cmake ../
     make -j6
 else
     echo "googletest already exists in libs/benchmark. To setup again, first remove libs/benchmark/googletest."
