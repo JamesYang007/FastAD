d04f0bd1779807ce302f4ed64c804b482a678c7f
James Yang <jamesyang916@gmail.com>
2019-12-25 23:21:04 -0500

    Update setup to update submodule always

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@
 # If setup.sh was called before
 if [ -d "libs/benchmark/googletest" ]; then
     rm -rf libs/benchmark
-    git submodule update --remote
 fi
 
+# Update submodule if needed
+git submodule update --remote
 # Setup google benchmark and googletest
 git clone https://github.com/google/googletest.git libs/benchmark/googletest
 cd libs/benchmark
