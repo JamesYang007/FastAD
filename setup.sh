e9beb3902ccd95e7e792cbb8d0aed47a864874f3
James Yang <jamesyang916@gmail.com>
2020-04-27 23:34:24 -0400

    Simpler setup for lib deps

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-# If setup.sh was called before
-if [ -d "libs/benchmark/googletest" ]; then
-    rm -rf libs/benchmark
-fi
+projectdir=$(dirname "BASH_SOURCE")
+gbenchpath="libs/benchmark"
+gtestpath="$gbenchpath/googletest"
 
 # Update submodule if needed
-git submodule update --remote
-# Setup google benchmark and googletest
-git clone https://github.com/google/googletest.git libs/benchmark/googletest
-cd libs/benchmark
-mkdir -p build && cd build
-cmake ../ -GNinja
-cmake --build . -- -j12
+git submodule update --init
+
+# setup googletest
+if [ ! -d "$gtestpath" ]; then
+    git clone https://github.com/google/googletest.git $gtestpath
+    cd $gtestpath 2>&1 /dev/null
+    git checkout -q release-1.10.0
+    cd ~- # change back to previous dir and no output to terminal
+fi
