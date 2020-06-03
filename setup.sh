2a161402789676abc8fcb9c7c4333e4446af5ac6
James Yang <jamesyang916@gmail.com>
2020-06-03 00:24:38 -0400

    Redirection work for bionic

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ git submodule update --init
 # setup googletest
 if [ ! -d "$gtestpath" ]; then
     git clone https://github.com/google/googletest.git $gtestpath
-    cd $gtestpath 2>&1 /dev/null
+    cd $gtestpath &> /dev/null
     git checkout -q release-1.10.0
     cd ~- # change back to previous dir and no output to terminal
 fi
